6ff07e80030f0ceefa00f5e25e9bb5a38fd606d3
lucasHaddad <lucasmh97@gmail.com>
2019-07-21 15:43:44 -0300

    Change img position and create dropdown sidebar menu.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <v-app id="app">
     <v-toolbar app id="menu">
-      <v-toolbar-side-icon>
-      </v-toolbar-side-icon>
-      <v-spacer></v-spacer>
+      <v-menu class="hidden-md-and-up">
+        <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+        <v-list id="side">
+          <v-btn to="/" flat>O EVENTO</v-btn>
+          <v-btn to="/about" flat>PROGRAMAÇÃO</v-btn>
+          <v-btn to="/promo" flat>INGRESSOS</v-btn>
+        </v-list>
+      </v-menu>
       <v-img id="logo" :src="require('./assets/logo.png')"/>
+      <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn to="/" flat>O EVENTO</v-btn>
         <v-btn to="/about" flat>PROGRAMAÇÃO</v-btn>
@@ -28,6 +34,11 @@ body {
   margin: 0 auto;
   overflow: hidden;
   background-image: url("./assets/background.png");
+}
+
+#side {
+  display: flex;
+  flex-direction: column;
 }
 
 #app {
