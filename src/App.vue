cec4cdb4c803ef217216a62582f22b62e7e29f68
lucasHaddad <lucasmh97@gmail.com>
2019-07-21 12:44:55 -0300

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-toolbar app id="menu">
+      <v-toolbar-side-icon>
+      </v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-img id="logo" :src="require('./assets/logo.png')"/>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-btn
+        color="success"
+        href="https://www.hbatools.com.br/MATSURI-POP-GEEK-2019__326"
+        target="_blank"
+      >
+        <span class="mr-2">COMPRAR INGRESSOS</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      //
+    };
+  },
+};
+</script>
+
+<style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-image: url("./assets/background.png");
 }
 </style>
