76d35af117e29f8dc5c5f35d900edd23161b7fa3
lucasHaddad <lucasmh97@gmail.com>
2019-07-24 02:35:46 -0300

    Add everything that is needed.

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,83 @@
 <template>
   <v-app id="app">
-    <div id="menu">
-      <v-toolbar app color=#FFF >
-        <v-menu class="hidden-md-and-up">
-          <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
-          <v-list id="side">
-            <v-btn to="/" flat>PROMOÇÃO</v-btn>
-            <v-btn to="/event" flat>O EVENTO</v-btn>
-            <v-btn to="/schedule" flat>PROGRAMAÇÃO</v-btn>
-            <v-btn to="/ticket" flat>INGRESSOS</v-btn>
-          </v-list>
-        </v-menu>
-        <div style="height: 100%;">
-          <img style="height: 100%;" id="logo" :src="require('./assets/logo.png')"/>
-        </div>
-        <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn to="/" flat>PROMOÇÃO</v-btn>
-          <v-btn to="/event" flat>O EVENTO</v-btn>
-          <v-btn to="/schedule" flat>PROGRAMAÇÃO</v-btn>
-          <v-btn to="/ticket" flat>INGRESSOS</v-btn>
-        </v-toolbar-items>
-        <v-spacer></v-spacer>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item-group color="primary">
+        <v-list-tile to="/">
+          <v-list-tile-action>
+            <v-icon>event</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>O EVENTO</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="/ticket">
+          <v-list-tile-action>
+            <v-icon>gavel</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>INGRESSOS</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="drawer = false" class="close">
+          <v-list-tile-action>
+            <v-icon>arrow_back</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>FECHAR MENU</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list-item-group>
+    </v-navigation-drawer>
+    <v-toolbar fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = true" class="hidden-md-and-up"/>
+      <div class="logo">
+        <img class="logo" :src="require('./assets/logo.png')"/>
+      </div>
+      <v-spacer/>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn to="/" flat>O EVENTO</v-btn>
+        <v-btn to="/ticket" flat>INGRESSOS</v-btn>
+      </v-toolbar-items>
+      <v-spacer/>
+      <v-flex>
         <v-btn
-          color="success"
-          href="https://www.hbatools.com.br/MATSURI-POP-GEEK-2019__326"
-          target="_blank"
+            color="success"
+            href="https://www.hbatools.com.br/MATSURI-POP-GEEK-2019__326"
+            target="_blank"
         >
-          <span class="mr-2">COMPRAR INGRESSOS</span>
+          COMPRAR INGRESSOS
         </v-btn>
-      </v-toolbar>
-    </div>
-    <div id="content">
-      <router-view></router-view>
-    </div>
+      </v-flex>
+    </v-toolbar>
+    <v-container>
+      <v-content>
+        <router-view/>
+      </v-content>
+    </v-container>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
 
 <style>
 body {
   margin: 0 auto;
   background-image: url("./assets/background.png");
   background-repeat: round;
+  height: 100%;
 }
 
-#side {
-  display: flex;
-  flex-direction: column;
-}
-
-#content {
-  top: 90px;
-  position: relative;
-  display: block;
+.logo {
+  height: 100%;
 }
 
 #menu {
@@ -64,7 +89,12 @@ body {
   background: inherit;
 }
 
-.v-toolbar {
+.close {
+  position: absolute;
+  bottom: 0px;
+}
+
+.v-toolbar,.v-navigation-drawer{
   font-family: 'Fjalla One', sans-serif;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Event from './views/Event.vue';
 
 Vue.use(Router);
 
@@ -10,18 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/event',
       name: 'event',
-      component: () => import('./views/Event.vue'),
-    },
-    {
-      path: '/schedule',
-      name: 'schedule',
-      component: () => import('./views/Schedule.vue'),
+      component: Event,
     },
     {
       path: '/ticket',
